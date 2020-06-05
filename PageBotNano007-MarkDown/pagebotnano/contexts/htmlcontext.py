8d01dcb72c4fe6f945916d03d6f2532876a0e810
petrvanblokland <buro@petr.com>
2020-06-06 00:04:16 +0200

    Working on HTML output

--- PageBotNano007-MarkDown/pagebotnano/contexts/htmlcontext.py
+++ PageBotNano007-MarkDown/pagebotnano/contexts/htmlcontext.py
@@ -34,16 +34,18 @@ class HtmlContext:
 		"""
 
 		>>> from pagebotnano.document import Document
-		>>> from pagebotnano.elements import Rect
+		>>> from pagebotnano.elements import Rect, Text
+		>>> from pagebotnano.babelstring import BabelString
 		>>> context = HtmlContext()
 		>>> doc = Document(context=context)
 		>>> page = doc.newPage()
 		>>> padding = 40
 		>>> e = Rect(padding, padding, page.w-2*padding, page.h-2*padding, fill=(1, 0.2, 1))
 		>>> page.addElement(e)
-
-		>>> page = doc.newPage()
-		>>> page = doc.newPage()
+		>>> style = dict(font='Georgia', fontSize=100)
+		>>> bs = BabelString('Hello world', style)
+		>>> e = Text(bs, padding, page.h/2, fill=(1, 0, 0))
+		>>> page.addElement(e)
 		>>> doc.export('_export/HtmlContext-website')
 		"""
 		self.newDrawing()
@@ -68,7 +70,10 @@ class HtmlContext:
 
 	def rect(self, x, y, w, h):
 		self.page['body'] += '<div width="%d"></div>' % w
-		
+
+	def text(self, bs, p):
+		self.page['body'] += '<p>%s</p>' % bs.html
+
 	def saveImage(self, path, multipage=True):
 		"""Create folder names `path` if it does not already exist.
 		"""

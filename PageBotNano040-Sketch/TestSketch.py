ac0a01db7499e5f8593be0337c61eac93158df9f
petrvanblokland <buro@petr.com>
2020-06-22 12:54:25 +0200

    Working in Nano implentation of Sketch connection

--- PageBotNano040-Sketch/TestSketch.py
+++ PageBotNano040-Sketch/TestSketch.py
@@ -25,21 +25,20 @@ print('SketchFile:', api.sketchFile) # <SketchFile path=TemplateSquare.sketch>
 print('Path:', api.sketchFile.path) # The api stores the path of the Sketch file
 
 sketchPage = api.selectPage(0) # SketchPage instance, not PageBotNano Page.
-print('SketchPage:', sketchPage) # SketchPage: <SketchPage name=Page 1>
-"""
->>> page, api.page
-(<SketchPage name=Page 1>, <SketchPage name=Page 1>)
->>> page.name
-'Page 1'
->>> len(page.layers[0]), page.artBoards, len(page.artBoards[0])
-(7, [<SketchArtboard name=Artboard 1 w=576 h=783>], 7)
->>> artBoard = page.artBoards[0]
->>> e = artBoard.layers[3]
->>> e, e.name
-(<SketchRectangle name=Rectangle Middle Right>, 'Rectangle Middle Right')
->>> e = page.find(pattern='Top Left')[0]
->>> e.name, e.frame
-('Rectangle Top Left', <SketchRect x=60 y=0 w=216 h=168>)
->>> #e.style['fills']
-"""
+print('Sketch Page:', sketchPage) # SketchPage: <SketchPage name=Page 1>
+print('Page name:', sketchPage.name) # 'Page 1' 
+# Since Sketch has pages with artboards, we'll use this extra layer
+# as follows:
+# sketchPage --> PageBotNano Document
+# stetchArtBoard --> PageBotNano Page
+artBoard = sketchPage.artBoards[0] 
+print('Sketch ArtBoard:', artBoard) # <SketchArtboard name=Artboard 1 w=576 h=783>
+# Get one of the rectangles on this artBoard
+e = artBoard.layers[3]
+print('Element[3] name:', e.name) # 'Rectangle Middle Right'
+# Find the element with this name
+e = sketchPage.find(pattern='Top Left')[0]
+print('Found element name:', e.name) # 'Rectangle Top Left'
+print('Found element frame:', e.frame) # <SketchRect x=60 y=0 w=216 h=168>
+
 print('Done')

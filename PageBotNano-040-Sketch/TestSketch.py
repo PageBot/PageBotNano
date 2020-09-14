8819a84ed59d6a27aa99f0478ac4af46e25f069a
Michiel <michiel@kauwatjoe.com>
2020-09-14 10:04:49 +0200

    Solving some Sketch issues.

--- PageBotNano-040-Sketch/TestSketch.py
+++ PageBotNano-040-Sketch/TestSketch.py
@@ -19,7 +19,7 @@
 from pysketch.sketchapi import SketchApi
 
 class SketchTester:
-    """The SketchTest app reads/writes from a template Sketch document. 
+    """The SketchTest app reads/writes from a template Sketch document.
 
     >>> path = 'pysketch/Resources/TemplateSquare.sketch'
     >>> api = SketchApi(path) # See pysketch/sketchapi.py for interface specs.
@@ -34,9 +34,9 @@ class SketchTester:
     'Page 1'
     >>> # Since Sketch has pages with artboards, we'll use this extra layer as follows:
     >>> # sketchPage --> PageBotNano Document
-    >>> sketchArtBoard --> PageBotNano Page
-    >>> sketchRectangle --> PageBotNano Rect element
-    >>> artBoard = sketchPage.artboards[0] 
+    >>> # sketchArtBoard --> PageBotNano Page
+    >>> # sketchRectangle --> PageBotNano Rect element
+    >>> artBoard = sketchPage.artboards[0]
     >>> artBoard
     <SketchArtboard name=Artboard 1 w=576 h=783>
     >>> # Get one of the rectangles on this artBoard
@@ -49,12 +49,12 @@ class SketchTester:
     >>> se
     <SketchRectangle name=Rectangle Top Left>
     >>> se.frame # SketchRect is a frame with position and size, different from SketchRectangle
-    <SketchRect x=60 y=96 w=216 h=168>
+    <SketchRect x=60 y=0 w=216 h=168>
     >>> se.frame.x += 400 # Move the rectangle.
-    
+
     """
 
 if __name__ == "__main__":
     # Running this document will execute all >>> comments as test of this source.
     import doctest
-    doctest.testmod()[0]    
+    doctest.testmod()[0]
